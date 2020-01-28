5ce186880b2977cdbd0468a410d470760fe0741f
Ryan Turner <turner@uber.com>
2020-01-27 17:23:48 -0800

    Logging response size of FetchX509SVID in SPIRE Agent

--- pkg/agent/client/client.go
+++ pkg/agent/client/client.go
@@ -6,6 +6,7 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"errors"
+	"github.com/gogo/protobuf/proto"
 	"io"
 	"net/url"
 	"sync"
@@ -126,6 +127,13 @@ func (c *client) FetchUpdates(ctx context.Context, req *node.FetchX509SVIDReques
 			c.c.Log.Warn("empty update in SVID update stream")
 			continue
 		}
+
+		c.c.Log.WithFields(logrus.Fields{
+			"response_size": proto.Size(resp),
+			"num_registration_entries": len(resp.SvidUpdate.RegistrationEntries),
+			"num_trust_bundles": len(resp.SvidUpdate.Bundles),
+			"num_svids": len(resp.SvidUpdate.Svids),
+		}).Debug("Received registration entries")
 
 		for _, re := range resp.SvidUpdate.RegistrationEntries {
 			if err := validateRegistrationEntry(re); err != nil {
