e12c315b0687df5f650b979065535003e2246b3a
Ryan Turner <turner@uber.com>
2020-05-28 16:50:43 -0700

    kv-datastore: implement remaining JoinToken RPCs
    Signed-off-by: Ryan Turner <turner@uber.com>

---
+++ pkg/server/plugin/datastore/kv/kvdatastore_jointoken_test.go
@@ -0,0 +1,184 @@
+package kv
+
+import (
+	"testing"
+	"time"
+
+	"github.com/spiffe/spire/pkg/server/plugin/datastore"
+)
+
+func (s *PluginSuite) TestCreateAndFetchJoinToken() {
+	now := time.Now().Unix()
+	joinToken := &datastore.JoinToken{
+		Token:  "foobar",
+		Expiry: now,
+	}
+
+	cReq := &datastore.CreateJoinTokenRequest{
+		JoinToken: joinToken,
+	}
+
+	_, err := s.ds.CreateJoinToken(ctx, cReq)
+	s.Require().NoError(err)
+
+	fReq := &datastore.FetchJoinTokenRequest{
+		Token: joinToken.Token,
+	}
+	res, err := s.ds.FetchJoinToken(ctx, fReq)
+	s.Require().NoError(err)
+	s.Equal("foobar", res.JoinToken.Token)
+	s.Equal(now, res.JoinToken.Expiry)
+}
+
+func (s *PluginSuite) TestCreateInvalidJoinToken() {
+	tests := []struct {
+		name        string
+		token       *datastore.JoinToken
+		expectedErr string
+	}{
+		{
+			name:        "nil join token",
+			expectedErr: "token is required",
+		},
+		{
+			name: "empty join token",
+			token: &datastore.JoinToken{
+				Expiry: 1000,
+			},
+			expectedErr: "token is required",
+		},
+		{
+			name: "expiry of zero",
+			token: &datastore.JoinToken{
+				Token:  "foo",
+				Expiry: 0,
+			},
+			expectedErr: "expiry is required",
+		},
+		{
+			name: "negative expiry",
+			token: &datastore.JoinToken{
+				Token:  "foo",
+				Expiry: -1,
+			},
+			expectedErr: "expiry is required",
+		},
+	}
+
+	for _, test := range tests {
+		s.T().Run(test.name, func(t *testing.T) {
+			ds := s.newPlugin()
+			cReq := &datastore.CreateJoinTokenRequest{
+				JoinToken: test.token,
+			}
+
+			_, err := ds.CreateJoinToken(ctx, cReq)
+			s.Assert().Error(err, test.expectedErr)
+		})
+	}
+}
+
+func (s *PluginSuite) TestDeleteJoinToken() {
+	now := time.Now().Unix()
+	joinToken1 := &datastore.JoinToken{
+		Token:  "foobar",
+		Expiry: now,
+	}
+
+	cReq1 := &datastore.CreateJoinTokenRequest{
+		JoinToken: joinToken1,
+	}
+
+	_, err := s.ds.CreateJoinToken(ctx, cReq1)
+	s.Require().NoError(err)
+
+	joinToken2 := &datastore.JoinToken{
+		Token:  "batbaz",
+		Expiry: now,
+	}
+
+	cReq2 := &datastore.CreateJoinTokenRequest{
+		JoinToken: joinToken2,
+	}
+
+	_, err = s.ds.CreateJoinToken(ctx, cReq2)
+	s.Require().NoError(err)
+
+	dReq := &datastore.DeleteJoinTokenRequest{
+		Token: joinToken1.Token,
+	}
+
+	_, err = s.ds.DeleteJoinToken(ctx, dReq)
+	s.Require().NoError(err)
+
+	fReq1 := &datastore.FetchJoinTokenRequest{
+		Token: joinToken1.Token,
+	}
+	// Should not be able to fetch after delete
+	resp, err := s.ds.FetchJoinToken(ctx, fReq1)
+	s.Require().NoError(err)
+	s.Nil(resp.JoinToken)
+
+	fReq2 := &datastore.FetchJoinTokenRequest{
+		Token: joinToken2.Token,
+	}
+
+	// Second token should still be present
+	resp, err = s.ds.FetchJoinToken(ctx, fReq2)
+	s.Require().NoError(err)
+	s.AssertProtoEqual(joinToken2, resp.JoinToken)
+}
+
+func (s *PluginSuite) TestPruneJoinTokens() {
+	now := time.Now().Unix()
+	joinToken := &datastore.JoinToken{
+		Token:  "foobar",
+		Expiry: now,
+	}
+
+	cReq := &datastore.CreateJoinTokenRequest{
+		JoinToken: joinToken,
+	}
+
+	_, err := s.ds.CreateJoinToken(ctx, cReq)
+	s.Require().NoError(err)
+
+	pReq1 := &datastore.PruneJoinTokensRequest{
+		ExpiresBefore: now - 10,
+	}
+
+	// Ensure we don't prune valid tokens, wind clock back 10s
+	_, err = s.ds.PruneJoinTokens(ctx, pReq1)
+	s.Require().NoError(err)
+
+	fReq := &datastore.FetchJoinTokenRequest{
+		Token: joinToken.Token,
+	}
+
+	resp, err := s.ds.FetchJoinToken(ctx, fReq)
+	s.Require().NoError(err)
+	s.Equal("foobar", resp.JoinToken.Token)
+
+	pReq2 := &datastore.PruneJoinTokensRequest{
+		ExpiresBefore: now,
+	}
+	// Ensure we don't prune on the exact ExpiresBefore
+	_, err = s.ds.PruneJoinTokens(ctx, pReq2)
+	s.Require().NoError(err)
+
+	resp, err = s.ds.FetchJoinToken(ctx, fReq)
+	s.Require().NoError(err)
+	s.Equal("foobar", resp.JoinToken.Token)
+
+	// Ensure we prune old tokens
+	joinToken.Expiry = now + 10
+	pReq3 := &datastore.PruneJoinTokensRequest{
+		ExpiresBefore: joinToken.Expiry,
+	}
+	_, err = s.ds.PruneJoinTokens(ctx, pReq3)
+	s.Require().NoError(err)
+
+	resp, err = s.ds.FetchJoinToken(ctx, fReq)
+	s.Require().NoError(err)
+	s.Nil(resp.JoinToken)
+}
--- pkg/server/plugin/datastore/kv/message/jointoken/handler.go
+++ pkg/server/plugin/datastore/kv/message/jointoken/handler.go
@@ -2,11 +2,12 @@ package jointoken
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
+	"github.com/gogo/protobuf/proto"
 	"github.com/spiffe/spire/internal/protokv"
 	"github.com/spiffe/spire/pkg/server/plugin/datastore"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 type handler struct {
@@ -20,19 +21,64 @@ func New(kv protokv.KV) Operations {
 }
 
 func (h *handler) Create(ctx context.Context, req *datastore.CreateJoinTokenRequest) (*datastore.CreateJoinTokenResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "not implemented")
+	// TODO: Prefer to send InvalidArgument code here, but keeping it this way for now to match the sql plugin behavior
+	if req.JoinToken == nil || req.JoinToken.Token == "" {
+		return nil, errors.New("token is required")
+	}
+
+	// TODO: Prefer to send InvalidArgument code here, but keeping it this way for now to match the sql plugin behavior
+	if req.JoinToken.Expiry <= 0 {
+		return nil, errors.New("expiry is required")
+	}
+
+	value, err := proto.Marshal(req.JoinToken)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := h.store.Create(ctx, value); err != nil {
+		return nil, err
+	}
+
+	return &datastore.CreateJoinTokenResponse{
+		JoinToken: req.JoinToken,
+	}, nil
 }
 
 func (h *handler) Delete(ctx context.Context, req *datastore.DeleteJoinTokenRequest) (*datastore.DeleteJoinTokenResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "not implemented")
+	in := &datastore.JoinToken{
+		Token: req.Token,
+	}
+
+	out := &datastore.JoinToken{}
+	if err := h.store.ReadProto(ctx, in, out); err != nil {
+		return nil, err
+	}
+
+	outBytes, err := proto.Marshal(out)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := h.store.Delete(ctx, outBytes); err != nil {
+		return nil, err
+	}
+
+	return &datastore.DeleteJoinTokenResponse{
+		JoinToken: out,
+	}, nil
 }
 
 func (h *handler) Fetch(ctx context.Context, req *datastore.FetchJoinTokenRequest) (*datastore.FetchJoinTokenResponse, error) {
-	in := &datastore.JoinToken{Token: req.Token}
-	out := new(datastore.JoinToken)
+	in := &datastore.JoinToken{
+		Token: req.Token,
+	}
+
+	out := &datastore.JoinToken{}
 	if err := h.store.ReadProto(ctx, in, out); err != nil {
+		// TODO: Prefer to send NotFound here, but keeping it this way for now to match the sql plugin behavior
 		if protokv.NotFound.Has(err) {
-			return nil, status.Errorf(codes.NotFound, err.Error())
+			return &datastore.FetchJoinTokenResponse{}, nil
 		}
 		return nil, err
 	}
@@ -41,5 +87,51 @@ func (h *handler) Fetch(ctx context.Context, req *datastore.FetchJoinTokenReques
 }
 
 func (h *handler) Prune(ctx context.Context, req *datastore.PruneJoinTokensRequest) (*datastore.PruneJoinTokensResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "not implemented")
+	var token []byte
+	var limit int
+	values, _, err := h.store.Page(ctx, token, limit)
+	if err != nil {
+		return nil, err
+	}
+
+	joinTokens := make([]*datastore.JoinToken, len(values))
+	for i, joinTokenBytes := range values {
+		joinToken := &datastore.JoinToken{}
+		if err := proto.Unmarshal(joinTokenBytes, joinToken); err != nil {
+			return nil, err
+		}
+		joinTokens[i] = joinToken
+	}
+
+	joinTokensToPrune := h.joinTokensToPrune(joinTokens, req.ExpiresBefore)
+	var errs []error
+	for _, joinToken := range joinTokensToPrune {
+		// TODO: Consider creating a "batch" Delete() API in protokv to minimize query load
+		joinTokenBytes, err := proto.Marshal(joinToken)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := h.store.Delete(ctx, joinTokenBytes); err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	numErrs := len(errs)
+	if numErrs > 0 {
+		return nil, fmt.Errorf("failed to prune %v join tokens", numErrs)
+	}
+
+	return &datastore.PruneJoinTokensResponse{}, nil
+}
+
+func (h *handler) joinTokensToPrune(joinTokens []*datastore.JoinToken, expiresBeforeSeconds int64) []*datastore.JoinToken {
+	var joinTokensToPrune []*datastore.JoinToken
+	for _, joinToken := range joinTokens {
+		if joinToken.Expiry < expiresBeforeSeconds {
+			joinTokensToPrune = append(joinTokensToPrune, joinToken)
+		}
+	}
+
+	return joinTokensToPrune
 }
