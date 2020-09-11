6503f077e9f81047bde94fde89d3b6cd31d96f09
Ryan Turner <turner@uber.com>
2020-09-11 01:09:34 -0700

    Use pagination in authorization crawl cache build
    Signed-off-by: Ryan Turner <turner@uber.com>

--- pkg/server/cache/entrycache/fullcache_ds.go
+++ pkg/server/cache/entrycache/fullcache_ds.go
@@ -4,8 +4,14 @@ import (
 	"context"
 
 	"github.com/spiffe/go-spiffe/v2/spiffeid"
+
 	"github.com/spiffe/spire/pkg/server/plugin/datastore"
 	"github.com/spiffe/spire/proto/spire/common"
+)
+
+const (
+	agentPageSize = 5000
+	entryPageSize = 5000
 )
 
 func BuildFromDataStore(ctx context.Context, ds datastore.DataStore) (*Cache, error) {
@@ -13,10 +19,11 @@ func BuildFromDataStore(ctx context.Context, ds datastore.DataStore) (*Cache, er
 }
 
 type entryIteratorDS struct {
-	ds      datastore.DataStore
-	entries []*common.RegistrationEntry
-	next    int
-	err     error
+	ds              datastore.DataStore
+	entries         []*common.RegistrationEntry
+	next            int
+	err             error
+	paginationToken string
 }
 
 func makeEntryIteratorDS(ds datastore.DataStore) EntryIterator {
@@ -29,15 +36,28 @@ func (it *entryIteratorDS) Next(ctx context.Context) bool {
 	if it.err != nil {
 		return false
 	}
-	if it.entries == nil {
-		resp, err := it.ds.ListRegistrationEntries(ctx, &datastore.ListRegistrationEntriesRequest{
+	if it.entries == nil || (it.next >= len(it.entries) && it.paginationToken != "") {
+		req := &datastore.ListRegistrationEntriesRequest{
 			TolerateStale: true,
-		})
+			Pagination: &datastore.Pagination{
+				Token:    it.paginationToken,
+				PageSize: entryPageSize,
+			},
+		}
+
+		resp, err := it.ds.ListRegistrationEntries(ctx, req)
 		if err != nil {
 			it.err = err
 			return false
 		}
+
+		if len(resp.Entries) == 0 {
+			return false
+		}
+
+		it.paginationToken = resp.Pagination.Token
 		it.entries = resp.Entries
+		it.next = 0
 	}
 	if it.next >= len(it.entries) {
 		return false
@@ -72,22 +92,9 @@ func (it *agentIteratorDS) Next(ctx context.Context) bool {
 		return false
 	}
 	if it.agents == nil {
-		resp, err := it.ds.ListNodeSelectors(ctx, &datastore.ListNodeSelectorsRequest{
-			TolerateStale: true,
-		})
-		if err != nil {
-			it.err = err
+		if anyAgents := it.fetchAgents(ctx); !anyAgents {
 			return false
 		}
-		agents := make([]Agent, 0, len(resp.Selectors))
-		for _, selector := range resp.Selectors {
-			agent := Agent{
-				ID:        spiffeid.RequireFromString(selector.SpiffeId),
-				Selectors: selector.Selectors,
-			}
-			agents = append(agents, agent)
-		}
-		it.agents = agents
 	}
 	if it.next >= len(it.agents) {
 		return false
@@ -102,4 +109,63 @@ func (it *agentIteratorDS) Agent() Agent {
 
 func (it *agentIteratorDS) Err() error {
 	return it.err
+}
+
+func (it *agentIteratorDS) fetchAgents(ctx context.Context) bool {
+	var agents []Agent
+	var token string
+	var currentID string
+	var selectors []*common.Selector
+	pushAgent := func(spiffeID string, curSelectors []*common.Selector) {
+		switch {
+		case currentID == "":
+			currentID = spiffeID
+		case spiffeID != currentID:
+			agent := Agent{
+				ID:        spiffeid.RequireFromString(currentID),
+				Selectors: selectors,
+			}
+			agents = append(agents, agent)
+			currentID = spiffeID
+			selectors = nil
+		}
+
+		for _, selector := range curSelectors {
+			selectors = append(selectors, selector)
+		}
+	}
+
+	for {
+		req := &datastore.ListNodeSelectorsRequest{
+			TolerateStale: true,
+			Pagination: &datastore.Pagination{
+				Token:    token,
+				PageSize: agentPageSize,
+			},
+		}
+
+		resp, err := it.ds.ListNodeSelectors(ctx, req)
+		if err != nil {
+			it.err = err
+			return false
+		}
+
+		if len(resp.Selectors) == 0 {
+			pushAgent("", nil)
+			if len(agents) == 0 {
+				return false
+			}
+
+			break
+		}
+
+		for _, selector := range resp.Selectors {
+			pushAgent(selector.SpiffeId, selector.Selectors)
+		}
+
+		token = resp.Pagination.Token
+	}
+
+	it.agents = agents
+	return true
 }
